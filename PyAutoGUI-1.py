40e78519cc93f2a1bc90e23d9766a2239916a729
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-03-10 23:26:17 +0530

    Add files via upload

---
+++ PyAutoGUI-1.py
@@ -0,0 +1,21 @@
+# Painting Bot using PyautoGUI
+
+import pyautogui
+import time
+import subprocess
+
+subprocess.Popen(r"C:/WINDOWS/system32/mspaint.exe")
+
+time.sleep(6)
+pyautogui.moveTo(300,300)
+
+pyautogui.click()
+distance = 300
+
+while distance > 0:
+    pyautogui.dragRel(distance, 0, duration = 0.1)
+    distance = distance - 5
+    pyautogui.dragRel(0, distance, duration = 0.1)
+    pyautogui.dragRel(-distance, 0, duration = 0.1)
+    distance = distance - 5
+    pyautogui.dragRel(0, -distance, duration = 0.1)
